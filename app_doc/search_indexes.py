d2d89fef3db02fe7b798445b220eb346a28e4539
Arise <arise@Mac-Studio.local>
2025-11-16 04:12:55 +0800

    perf：优化全文搜索的逻辑，根据编辑模式去匹配不同的索引字段（动态索引）
    因为：   当 editor_mode = 1 或 2 时，只填充pre_content字段，当 editor_mode = 3，只填充content字段
    所以：需要优化搜索的匹配逻辑

--- app_doc/search_indexes.py
+++ app_doc/search_indexes.py
@@ -15,13 +15,30 @@ class DocIndex(indexes.SearchIndex,indexes.Indexable):
     # 独立的标题字段（用于标题搜索，权重更高）
     title = indexes.CharField(model_attr='name')
 
-    # 独立的内容字段（用于内容搜索）
-    content = indexes.CharField(model_attr='content')
+    # 独立的内容字段（根据编辑器模式动态选择字段）
+    content = indexes.CharField()
 
     # 元数据字段
     top_doc = indexes.IntegerField(model_attr='top_doc')
     modify_time = indexes.DateTimeField(model_attr='modify_time')
     create_user = indexes.IntegerField(model_attr='create_user_id')
+
+    def prepare_content(self, obj):
+        """
+        根据文档的编辑器模式动态返回内容字段
+        - editor_mode = 1 或 2 (Markdown编辑器): 返回 pre_content
+        - editor_mode = 3 (富文本编辑器): 返回 content
+        - 其他情况: 返回空字符串
+        """
+        if obj.editor_mode in [1, 2]:
+            # Markdown编辑器，使用pre_content
+            return obj.pre_content or ''
+        elif obj.editor_mode == 3:
+            # 富文本编辑器，使用content
+            return obj.content or ''
+        else:
+            # 其他编辑器类型，返回空
+            return ''
 
     def get_model(self):
         return Doc
